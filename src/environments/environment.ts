2a1a487100857e412308854ddc5fe928c8a4507a
AnthonyKeith <52044937+AnthonyKeith@users.noreply.github.com>
2023-06-26 00:24:53 +0100

    Weather App
    Dublin weather app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: '522df93ac0d45376d2de3b7ee8bf07cc',
+  API_URL: 'https://api.openweathermap.org/data/2.5/'
 };
 
 /*
